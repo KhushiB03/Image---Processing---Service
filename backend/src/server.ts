db90fad1e632a7c23614c850f985c544332d6fcb
KhushiB03 <ikhushi205@gmail.com>
2026-07-02 22:55:39 +0530

    err

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import DBconnect from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 DBconnect();
-console.log(process.env.CLOUDINARY_API_KEY);
+// console.log(process.env.CLOUDINARY_API_KEY);
 app.listen(PORT , ()=>{
     console.log(`Server running on port ${PORT}`);
 });
